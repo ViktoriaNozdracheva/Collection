353f9884719bca6f3a4b8ff3faf71b86e57346bb
Victoria <nozdracheva1997@mail.ru>
2018-05-07 10:28:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Cource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Cource : Predok
+    {
+
+        public Cource(string Organization, string Level, string Name)
+        {
+            this.Organization = Organization;
+            this.Level = Level;
+            this.Name = Name;
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Predok> list = new List<Predok>();
+
+            Cource Obj = new Cource(Organization: "Epam Рязань", Level: "Внутренние курсы", Name: "Автоматизированное тестирование ПО");
+            list.Add(Obj);
+
+            Cource O = new Cource(Organization: "Epam Рязань", Level: "Внешние курсы", Name: "Автоматизированное тестирование ПО");
+            list.Add(O);
+
+            foreach (Cource c in list)
+                c.InfoPr();
+
+            list.Sort(Level);
+
+            Console.WriteLine("Отсортированно: ");
+            foreach (Cource c in list)
+                c.InfoPr();
+
+            Console.ReadKey();
+
+        }
+
+        private static int Level(Predok x, Predok y)
+        {
+            return x.Level.CompareTo(y.Level);
+        }
+    }
+}
